65bca851f3471d33a56f764846fbf16d629fdfaf
Faceless <gideontriumph@gmail.com>
2024-01-25 14:43:09 -0700

    PyGame

---
+++ pygame.py
@@ -0,0 +1,23 @@
+"""
+    Name: pygame.py
+    Author: Triumph Ogbonnia
+    Created: 1/25/24
+    Purpose: My first platforming game with python
+"""
+
+# Import the pygame module
+import pygame
+
+# Import pygame.locals for easier access to key coordinates
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+# Initialize pygame
+pygame.init()
